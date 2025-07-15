23f8916afa04e51466029d64e8fc3691b3cacca2
안승우 <rio319@naver.com>
2025-07-15 15:59:27 +0900

    #04 feat: MultiSelector 구현

--- design-system/src/main/java/com/ieum/design_system/selector/SelectorState.kt
+++ design-system/src/main/java/com/ieum/design_system/selector/SelectorState.kt
@@ -16,6 +16,10 @@ interface ISingleSelectorState<T> : ISelectorState<T> {
     val selectedItem: T?
 }
 
+interface IMultipleSelectorState<T> : ISelectorState<T> {
+    val selectedItemList: List<T>
+}
+
 class SingleSelectorState<T>(
     override val itemList: List<T>
 ) : ISingleSelectorState<T> {
@@ -24,10 +28,34 @@ class SingleSelectorState<T>(
     override val selectedItem: T? get() = _selectedItem
 
     override fun selectItem(item: T) {
-        _selectedItem = if (isSelected(item)) null else item
+        _selectedItem = if (isSelected(item)) {
+            null
+        } else {
+            item
+        }
     }
 
     override fun isSelected(item: T): Boolean {
         return item == selectedItem
+    }
+}
+
+class MultipleSelectorState<T>(
+    override val itemList: List<T>
+) : IMultipleSelectorState<T> {
+    private var _selectedItemList by mutableStateOf<List<T>>(emptyList())
+
+    override val selectedItemList: List<T> get() = _selectedItemList
+
+    override fun selectItem(item: T) {
+        _selectedItemList = if (isSelected(item)) {
+            selectedItemList - item
+        } else {
+            selectedItemList + item
+        }
+    }
+
+    override fun isSelected(item: T): Boolean {
+        return item in selectedItemList
     }
 }
