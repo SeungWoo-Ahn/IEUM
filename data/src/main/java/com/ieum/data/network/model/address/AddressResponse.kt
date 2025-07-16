790c16e3a4395c86b3ba81ab74b13c423b7c3670
안승우 <rio319@naver.com>
2025-07-16 22:06:16 +0900

    #07 feat: AddressDataSource 구현

--- data/src/main/java/com/ieum/data/network/model/address/AddressResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.ieum.data.network.model.address
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AddressResponse(
-    val result: List<AddressDto>,
-)
--- data/src/main/java/com/ieum/data/network/model/address/AddressTokenResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.ieum.data.network.model.address
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AddressTokenResponse(
-    val result: SGISToken,
-)
