6c2020144fb38f490fb5430d9915f4a47e07f58d
안승우 <rio319@naver.com>
2025-07-16 23:41:33 +0900

    #07 feat: UserDataSource 구현

---
+++ data/src/main/java/com/ieum/data/datasource/user/UserRemoteDataSource.kt
@@ -0,0 +1,30 @@
+package com.ieum.data.datasource.user
+
+import com.ieum.data.network.di.IEUMNetwork
+import com.ieum.data.network.di.NetworkSource
+import com.ieum.data.network.model.user.RegisterRequestBody
+import com.ieum.data.network.model.user.UserDto
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import javax.inject.Inject
+
+class UserRemoteDataSource @Inject constructor(
+    @NetworkSource(IEUMNetwork.Default)
+    private val ktorClient: HttpClient,
+) : UserDataSource {
+    override suspend fun register(registerRequestBody: RegisterRequestBody) {
+        ktorClient
+            .post("api/v1/users/register") {
+                setBody(registerRequestBody)
+            }
+    }
+
+
+    override suspend fun getMyProfile(): UserDto =
+        ktorClient
+            .get("api/v1/users/profile")
+            .body<UserDto>()
+}
