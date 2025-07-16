7946c8bea3e6589d50effc0e0841f962325d3b69
안승우 <rio319@naver.com>
2025-07-16 20:36:30 +0900

    #07 feat: RepositoryImpl di

---
+++ data/src/main/java/com/ieum/data/repository/di/DataModule.kt
@@ -0,0 +1,30 @@
+package com.ieum.data.repository.di
+
+import com.ieum.data.repository.AddressRepositoryImpl
+import com.ieum.data.repository.AuthRepositoryImpl
+import com.ieum.data.repository.UserRepositoryImpl
+import com.ieum.domain.repository.AddressRepository
+import com.ieum.domain.repository.AuthRepository
+import com.ieum.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import javax.inject.Singleton
+
+@Module
+@Singleton
+internal interface DataModule {
+    @Binds
+    fun bindsAddressRepository(
+        addressRepository: AddressRepositoryImpl,
+    ) : AddressRepository
+
+    @Binds
+    fun bindsAuthRepository(
+        authRepository: AuthRepositoryImpl,
+    ): AuthRepository
+
+    @Binds
+    fun bindsUserRepository(
+        userRepository: UserRepositoryImpl,
+    ) : UserRepository
+}
