e10ec91cc7b62010487a1fe0c7fe221465aefc55
안승우 <rio319@naver.com>
2025-07-17 13:20:18 +0900

    #07 feat: protobuf 의존성 추가, tokenEntity proto 파일 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -4,34 +4,40 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.protobuf)
 }
 
 android {
     namespace = "com.ieum.data"
     compileSdk = 35
-
+    
     defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
     }
 }
 
@@ -49,5 +55,5 @@ dependencies {
     implementation(libs.kotlinx.serialization)
 
     // datastore
-    implementation(libs.androidx.datastore.proto)
+    implementation(libs.bundles.datastore)
 }
