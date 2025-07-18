8d19ed5862b2613096152e2e3be8d2780deee194
안승우 <rio319@naver.com>
2025-07-18 17:20:59 +0900

    #09 feat: Screen에 Serializable 추가

--- presentation/src/main/java/com/ieum/presentation/navigation/ScreenGraph.kt
+++ presentation/src/main/java/com/ieum/presentation/navigation/ScreenGraph.kt
@@ -1,7 +1,11 @@
 package com.ieum.presentation.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed interface ScreenGraph {
+    @Serializable
     data object Auth : ScreenGraph
 
+    @Serializable
     data object Main : ScreenGraph
 }
