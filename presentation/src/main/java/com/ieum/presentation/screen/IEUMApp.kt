7a82ebd7d38002c1e09c253b197bb1219099dc62
안승우 <rio319@naver.com>
2025-07-19 17:04:41 +0900

    #09 feat: statusBar, navigationBar padding 추가

--- presentation/src/main/java/com/ieum/presentation/screen/IEUMApp.kt
+++ presentation/src/main/java/com/ieum/presentation/screen/IEUMApp.kt
@@ -2,6 +2,8 @@ package com.ieum.presentation.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.ieum.presentation.navigation.IEUMNavHost
@@ -13,6 +15,8 @@ fun IEUMApp(
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding(),
     ) {
         IEUMNavHost(appState = appState)
     }
