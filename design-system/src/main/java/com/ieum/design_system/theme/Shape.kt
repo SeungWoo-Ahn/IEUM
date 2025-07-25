161cdaeccd6aa3d01b6feeb3afc0f71c05169a63
안승우 <rio319@naver.com>
2025-07-25 16:09:12 +0900

    #13 ui: Shape 선언

---
+++ design-system/src/main/java/com/ieum/design_system/theme/Shape.kt
@@ -0,0 +1,9 @@
+package com.ieum.design_system.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+internal val IEUMShapes = Shapes(
+    medium = RoundedCornerShape(16.dp)
+)
