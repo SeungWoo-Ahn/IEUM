a62d73962a3144801ade0005b01c87bbd7137465
안승우 <rio319@naver.com>
2025-07-25 15:46:39 +0900

    #13 ui: Color 선언

---
+++ design-system/src/main/java/com/ieum/design_system/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.ieum.design_system.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Gray50 = Color(0xFFF9FAFB)
+val Gray100 = Color(0xFFF3F4F6)
+val Gray200 = Color(0xFFE5E7EB)
+val Gray300 = Color(0xFFD1D5DC)
+val Gray500 = Color(0xFF6A7282)
+val Gray600 = Color(0xFF4A5565)
+val Gray950 = Color(0xFF030712)
+
+val Lime100 = Color(0xFFECFCCA)
+val Lime200 = Color(0xFFD8F999)
+val Lime400 = Color(0xFF7CCF00)
+val Lime500 = Color(0xFF7CCF00)
+
+val Slate100 = Color(0xFFF1F5F9)
+val Slate300 = Color(0xFFCAD5E2)
+val State900 = Color(0xFF0F172B)
