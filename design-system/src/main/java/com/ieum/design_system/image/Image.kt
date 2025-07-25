6aed5c268750f9d5d8ed2af4154ff28c452e75c7
안승우 <rio319@naver.com>
2025-07-25 16:52:44 +0900

    #13 ui: 이미지 추가

---
+++ design-system/src/main/java/com/ieum/design_system/image/Image.kt
@@ -0,0 +1,17 @@
+package com.ieum.design_system.image
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.ieum.design_system.R
+
+@Composable
+fun LoginBackGroundImage() {
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        painter = painterResource(R.drawable.login_background),
+        contentDescription = "login-background",
+    )
+}
