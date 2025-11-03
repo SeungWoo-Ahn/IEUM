6b5e51b7537e244ed4c3b504d0c413e3cb63ad6f
안승우 <rio319@naver.com>
2025-11-03 18:50:38 +0900

    #22 feat: UserRepository Post 관련 UseCase 작성

---
+++ domain/src/main/java/com/ieum/domain/usecase/user/GetOtherPostListFlowUseCase.kt
@@ -0,0 +1,15 @@
+package com.ieum.domain.usecase.user
+
+import androidx.paging.PagingData
+import com.ieum.domain.model.post.Post
+import com.ieum.domain.model.post.PostType
+import com.ieum.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetOtherPostListFlowUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke(id: Int, type: PostType): Flow<PagingData<Post>> =
+        userRepository.getOthersPostListFlow(id, type)
+}
