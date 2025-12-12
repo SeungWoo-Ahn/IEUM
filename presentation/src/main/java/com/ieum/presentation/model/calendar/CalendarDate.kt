2ee9f0a38ae78c963a69b8d9e064311fd0548dee
안승우 <rio319@naver.com>
2025-12-12 23:44:49 +0900

    #32 feat: 캘린더 관련 data class 선언

---
+++ presentation/src/main/java/com/ieum/presentation/model/calendar/CalendarDate.kt
@@ -0,0 +1,7 @@
+package com.ieum.presentation.model.calendar
+
+data class CalendarDate(
+    val year: Int,
+    val month: Int,
+    val dayOfMonth: Int,
+)
---
+++ presentation/src/main/java/com/ieum/presentation/model/calendar/CalendarMonth.kt
@@ -0,0 +1,12 @@
+package com.ieum.presentation.model.calendar
+
+data class CalendarMonth(
+    val year: Int,
+    val month: Int,
+    val numberOfDays: Int,
+    val daysFromStartOfWeekToFirstOfMonth: Int,
+) {
+    fun indexIn(years: IntRange): Int {
+        return (year - years.first) * 12 + month - 1
+    }
+}
